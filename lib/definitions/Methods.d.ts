12ca15d15687b66ad5323fdcfa86704d24439d7c
Liang, Jiapei <jliang@microstrategy.com>
2023-01-24 16:55:10 -0500

    include source files

---
+++ lib/definitions/Methods.d.ts
@@ -0,0 +1,17 @@
+declare const methods: string[]
+export default methods
+export declare type Methods =
+  | 'log'
+  | 'debug'
+  | 'info'
+  | 'warn'
+  | 'error'
+  | 'table'
+  | 'clear'
+  | 'time'
+  | 'timeEnd'
+  | 'count'
+  | 'assert'
+  | 'command'
+  | 'result'
+  | 'dir'
---
+++ lib/src/Transform/index.ts
@@ -0,0 +1,23 @@
+import { Message } from '../definitions/Console'
+import Arithmetic from './arithmetic'
+import Function from './Function'
+import HTML from './HTML'
+import Map from './Map'
+
+import Replicator from './replicator'
+
+const transforms = [HTML, Function, Arithmetic, Map]
+
+const replicator = new Replicator()
+replicator.addTransforms(transforms)
+
+export function Encode<T>(data: any, limit?: number): T {
+  return JSON.parse(replicator.encode(data, limit))
+}
+
+export function Decode(data: any): Message {
+  const decoded = replicator.decode(JSON.stringify(data))
+  // remove __console_feed_remaining__
+  decoded.data.pop()
+  return decoded
+}
